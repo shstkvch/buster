6c0f44f8b813124ab4bd420a1dd0aba9b7e59dc0
David Hewitson <david@Davids-iMac.local>
2013-08-28 12:35:19 +0100

    started on compilation tests

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    // config for the Buster Toolbelt (CLI for creating new sites)
+    toolbelt: {
+        
+    }
+}
+
+module.exports = config;
---
+++ toolbelt.js
@@ -0,0 +1,1 @@
+require("./lib/toolbelt/main");
